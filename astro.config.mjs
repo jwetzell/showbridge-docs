024cb28161799dd1ee48667af5a0610da1906059
Joel Wetzell <git@jwetzell.com>
2026-05-17 21:17:07 -0500

    add site option for sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://docs.showbridge.io",
   integrations: [
     starlight({
       title: "showbridge",
